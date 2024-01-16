3504a9773bfafd1fa271d0320eeab25e60506d10
ZanyarAzad <zanyar.200190011438@spu.edu.iq>
2024-01-16 23:28:25 +0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kPharma/app.dart';
+import 'package:kPharma/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
